dd1590713bc71b6844cb36430a092d2cdd4617b5
Eduardo Costa Rodrigues <edu.crod@gmail.com>
2021-05-06 23:56:07 -0300

    Add project files.

---
+++ SwordDamageProject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SwordDamageProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SwordDamage swordDamage = new SwordDamage();
+            Random random = new Random();
+            while (true)
+            {
+                Console.Write("0 for no magic/flaming, 1 for magic, 2 for flaming, 3 for both, anything else to quit: ");
+                char keyChar = Console.ReadKey(false).KeyChar;
+
+                if (keyChar != '0' && keyChar != '1' && keyChar != '2' && keyChar != '3') return;
+
+                int roll = random.Next(3, 18);
+
+                swordDamage.Roll = roll;
+                swordDamage.SetMagic(keyChar == '1' || keyChar == '3');
+                swordDamage.SetFlaming(keyChar == '2' || keyChar == '3');
+
+                Console.WriteLine($"\nRolled {roll} for {swordDamage.Damage} HP");
+            }
+        }
+    }
+}
